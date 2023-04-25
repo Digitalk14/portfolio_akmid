c57be858286cf4384668c63cd4280b8f31922ab3
Dmitrii Vasilev <akmid@MacBook-Pro-Dmitrii-2.local>
2023-04-24 17:59:46 -0700

    feat: Update svg icons

--- src/shared/utils/asset-prefix/asset-prefix.ts
+++ src/shared/utils/asset-prefix/asset-prefix.ts
@@ -1,9 +1,9 @@
 export const assetPrefix = () => {
-  const isGithubActions = process.env.GITHUB_ACTIONS || false;
-  const repo = process.env.GITHUB_REPOSITORY || false;
-  let assetPrefix = "";
-  if (isGithubActions && repo) {
-    assetPrefix = `/${repo.replace(/.*?\//, "")}`;
-  }
+  // const isGithubActions = process.env.GITHUB_ACTIONS || false;
+  // const repo = process.env.GITHUB_REPOSITORY || false;
+  // let assetPrefix = "";
+  // if (isGithubActions && repo) {
+  //   assetPrefix = `/${repo.replace(/.*?\//, "")}`;
+  // }
   return 'portfolio_akmid'
 };
