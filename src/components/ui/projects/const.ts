75647a45f2fb9b3b4de827a82f6fb5b84b6be335
digitalk14 <vasilievdmtr@gmail.com>
2025-03-07 17:34:17 -0800

    fix: Images paths

--- src/components/ui/projects/const.ts
+++ src/components/ui/projects/const.ts
@@ -5,19 +5,19 @@ export const PROJECTS = [
     title: "Cactus English Website",
     description:
       "A website for Kate, a CELTA-certified English teacher with 11+ years of experience, dedicated to helping students of all levels improve their English skills with engaging and effective lessons.",
-    img: "cactus-english-screenshot.jpg",
+    img: "cactus-english-screenshot",
     link: "https://www.cactus-english.com",
   },
   {
     title: "Canadian Grocery Alternatives",
     description: "Canadian Grocery Alternatives helps you discover local substitutes for your favorite grocery items, supporting Canadian businesses and sustainable shopping.",
-    img: "canadian_grocery_alternatives.jpg",
+    img: "canadian_grocery_alternatives",
     link: "https://www.grocery-alternatives.ca",
   },
   {
     title: "NumDoku",
     description: "NumDoku is a logic-based game where you guess a unique 4-digit secret number. With each attempt, you receive clues on correct digits and their positions. Solve the puzzle in 8 tries to win!",
-    img: "numdoku_screenshot.jpg",
+    img: "numdoku_screenshot",
     link: "https://numdoku.vercel.app",
   },
 ];
--- src/components/ui/projects/projects.tsx
+++ src/components/ui/projects/projects.tsx
@@ -21,30 +21,35 @@ export const Projects = ({
       id="projects"
       className="flex flex-col gap-4 min-h-[100vh] scroll-mt-[100px]"
     >
-      <p className="md:hidden mb-4 text-xl font-bold text-[#0d9272]">Projects</p>
-      {PROJECTS.map(({ title, description, img, link }) => (
-        <a
-          key={title}
-          href={link}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex flex-col-reverse md:flex-row gap-0 mb-6 md:mb-0 md:gap-4 md:p-4 rounded-md hover:bg-[#a9a9a9] transition-all duration-300"
-        >
-          <div className="w-full md:w-1/4">
-            <Image
-              src={`/projects/${img}`}
-              alt={title}
-              width={120}
-              height={100}
-              className="w-1/2 md:w-full object-cover rounded-md"
-            />
-          </div>
-          <div className="flex flex-col gap-2 w-full mb-4 md:mb-0 md:w-3/4">
-            <h3 className="text-[#0d9272] font-bold">{title}</h3>
-            <p>{description}</p>
-          </div>
-        </a>
-      ))}
+      <p className="md:hidden mb-4 text-xl font-bold text-[#0d9272]">
+        Projects
+      </p>
+      {PROJECTS.map(({ title, description, img, link }) => {
+        const imagePath = `/projects/${img}.jpg`;
+        return (
+          <a
+            key={title}
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex flex-col-reverse md:flex-row gap-0 mb-6 md:mb-0 md:gap-4 md:p-4 rounded-md hover:bg-[#a9a9a9] transition-all duration-300"
+          >
+            <div className="w-full md:w-1/4">
+              <Image
+                src={imagePath}
+                alt={title}
+                width={120}
+                height={100}
+                className="w-1/2 md:w-full object-cover rounded-md"
+              />
+            </div>
+            <div className="flex flex-col gap-2 w-full mb-4 md:mb-0 md:w-3/4">
+              <h3 className="text-[#0d9272] font-bold">{title}</h3>
+              <p>{description}</p>
+            </div>
+          </a>
+        );
+      })}
       <div ref={ref} className="h-[5px]"></div>
     </div>
   );
